6bd523e682d9b797e6f7ab1238d0707c20540c34
Jan Macháček <machacek.j@gmail.com>
2014-08-08 08:49:15 +0200

    Commandy pro testovani rabbitmq

---
+++ app/libs/ConsoleCommand/ProducerCommand.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jan
+ * Date: 8.8.14
+ * Time: 8:43
+ */
+
+namespace ConsoleCommand;
+
+
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ProducerCommand extends Command
+{
+
+	protected function configure()
+	{
+		$this
+			->setName('admin:producer')
+			->setDescription('Creates test consumer');
+//			->addArgument('name', InputArgument::REQUIRED, 'Producer Name')
+//			->addOption('debug', 'd', InputOption::VALUE_OPTIONAL, 'Enable Debugging', false);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+		$channel = $connection->channel();
+		$channel->queue_declare('hello', false, false, false, false);
+		$msg = new AMQPMessage('Hello World!');
+		$channel->basic_publish($msg, "", "hello");
+	}
+}
